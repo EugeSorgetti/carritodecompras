7d3fd52c57b9e468fadd04e8620f2ba389d88027
EugeSorgetti <eugesorgetti@gmail.com>
2021-07-02 23:48:22 -0300

    localStorage y pagos

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const footerTemplate = document.getElementById('footer-template').content;
 const cartTemplate = document.getElementById('cart-template').content;
 const fragment = document.createDocumentFragment();
 
+
 let cart = {};
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -20,12 +21,12 @@ items.addEventListener('click', (e) => {
     btnAction(e);
 })
 
-const fetchData = async() => {
+const fetchData = async () => {
     try {
         const res = await fetch('api.json');
         const data = await res.json();
         renderCard(data);
-    }catch(error) {
+    } catch (error) {
         console.log(error);
     }
 }
@@ -38,13 +39,13 @@ const renderCard = data => {
         template.querySelector('button').dataset.id = product.id;
         const clone = template.cloneNode(true);
         fragment.appendChild(clone);
-    }) ;
+    });
     cards.appendChild(fragment);
 }
 
 const addToCart = e => {
-    if(e.target.classList.contains('custom-btn')) {
-    
+    if (e.target.classList.contains('custom-btn')) {
+
         setCart(e.target.parentElement);
     }
     e.stopPropagation();
@@ -58,10 +59,12 @@ const setCart = obj => {
         quantity: 1
 
     }
-    if(cart.hasOwnProperty(product.id)) {
+    if (cart.hasOwnProperty(product.id)) {
         product.quantity = cart[product.id].quantity + 1;
     }
-    cart[product.id] = {...product};
+    cart[product.id] = {
+        ...product
+    };
     renderCart();
 }
 
@@ -89,8 +92,13 @@ const renderFooter = () => {
         footer.innerHTML = `<th scope="row" colspan="5">Carrito vacío - comience a comprar!</th>`
         return
     }
-    const nQuantity = Object.values(cart).reduce((acc, {quantity}) => acc+quantity, 0 );
-    const nPrice = Object.values(cart).reduce((acc, {quantity, price}) => acc+quantity*price, 0);
+    const nQuantity = Object.values(cart).reduce((acc, {
+        quantity
+    }) => acc + quantity, 0);
+    const nPrice = Object.values(cart).reduce((acc, {
+        quantity,
+        price
+    }) => acc + quantity * price, 0);
 
     footerTemplate.querySelectorAll('td')[0].textContent = nQuantity;
     footerTemplate.querySelector('span').textContent = nPrice;
@@ -104,25 +112,37 @@ const renderFooter = () => {
     emptyBtn.addEventListener('click', () => {
         cart = {};
         renderCart();
+    });
+
+    const endBtn = document.getElementById('end');
+    endBtn.addEventListener('click', () => {
+        cart = JSON.stringify(cart);
+        localStorage.setItem('totalProducts', cart);
+        console.log(localStorage);
+        return window.location.assign('end.html');
     })
 }
 
 const btnAction = e => {
-  
-    if(e.target.classList.contains('btn-dark')) {
+
+    if (e.target.classList.contains('btn-dark')) {
         const product = cart[e.target.dataset.id];
         product.quantity++;
-        cart[e.target.dataset.id] = {...product};
+        cart[e.target.dataset.id] = {
+            ...product
+        };
         console.log(cart);
         renderCart();
     }
-    if(e.target.classList.contains('btn-danger')) {
+    if (e.target.classList.contains('btn-danger')) {
         const product = cart[e.target.dataset.id];
         product.quantity--;
-        if(product.quantity === 0) {
+        if (product.quantity === 0) {
             delete cart[e.target.dataset.id];
         }
         renderCart();
     }
     e.stopPropagation();
 }
+
+
